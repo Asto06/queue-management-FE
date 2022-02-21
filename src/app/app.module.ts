cd080b95c45aa5e656c220b7a8aa261d906de023
Asto06 <kinantanoas@gmail.com>
2022-02-21 15:25:49 +0700

    Queue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputVisitorDetailsComponent } from './input-visitor-details/input-visitor-details.component';
+import { RequestQueueNumberComponent } from './request-queue-number/request-queue-number.component';
+import { MenuComponent } from './menu/menu.component';
+import { MaterialModule } from './tools/material.module';
+import { APP_BASE_HREF } from '@angular/common';
+import { VisitorListComponent } from './visitor-list/visitor-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { SidebarComponent } from './menu/sidebar/sidebar.component';
+import { NavbarComponent } from './menu/navbar/navbar.component';
+import { DashboardComponent } from './menu/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InputVisitorDetailsComponent,
+    RequestQueueNumberComponent,
+    VisitorListComponent,
+    SidebarComponent,
+    NavbarComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MaterialModule,
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
